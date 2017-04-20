b64bf6e6941bfb29d76b8fa8e47d1a99937b835b
rinziv <sax@epimelesi.isti.cnr.it>
2017-04-20 12:07:07 +0200

    Group data by museum counting paintings for each place

--- geo_dipinti/assets/main.js
+++ geo_dipinti/assets/main.js
@@ -1,7 +1,12 @@
 
+
+
+
+// DATA PREPARATION
+
 var dsv = d3.dsv(";","text/plain");
 
-
+var museums = {};
 
 d3.queue()
     .defer(dsv, "assets/data/opere_colori.csv", function(d){
@@ -24,20 +29,31 @@ d3.queue()
             
             // discard all the others
         }
+        
+        // save the coordinates of museum in the dictionary 
+        var ms = museums[d.MUSEUM] || (museums[d.MUSEUM] = {name: d.MUSEUM, lat: +d.ARTWORK_PLACE_LAT, lon: +d.ARTWORK_PLACE_LON});
+        
+        
         // return the modified row
         return m;
     })
 .await(callback);
 
 
-
-
 function callback(error, opere){
     if(error) console.log(error);
     
     console.log(opere);
+    console.log(museums);
     
     
+    // GROUP opere BY MUSEUM
+    var nested_all = d3.nest()
+        .key(function(d){return d.MUSEUM})
+        .rollup(function(leaves){return leaves.length})
+    .entries(opere);
     
+    console.log(nested_all);
+
 
 }
