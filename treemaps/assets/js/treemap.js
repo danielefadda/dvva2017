8f60f671d0135fe1408efec42ee6e2c7b5c05799
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2017-06-13 21:14:49 +0200

    Improvements to visualization

--- treemaps/assets/js/treemap.js
+++ treemaps/assets/js/treemap.js
@@ -20,6 +20,9 @@ function Treemap(){
 		
 		var treemap = d3.layout.treemap()
 			.size([config.width,config.height])
+			.padding(function(d){
+				return d.depth==2 ? [20,0,0,0] : 3
+			})
 		.value(function(d){return d[1]});
 		
 		console.log(treemap.nodes(selection.datum()));
@@ -32,11 +35,19 @@ function Treemap(){
 		.attr("transform", function(d){return "translate("+d.x+","+d.y+")"});
 		
 		cell.append("rect")
+			.classed("leaf", function(d){return !d.children})
 			.attr("width", function(d){return d.dx})
 			.attr("height", function(d){return d.dy})
 			.style("fill", function(d){return d.depth==1 ? color(d.name): null})
 		
-		// .text(function(d){return d.children ? null: d[0]});
+		
+		cell.append("text")
+			.attr("x", function(d){return d.dx / 2})
+			.attr("y", function(d){return d.dy / 2})
+			.attr("dy", ".3em")
+			.attr("text-anchor","middle")
+			.text(function(d){return (d.children) ? null: d[0]})
+;
 		
 	}
 	
